5c6f40ad3ad4086573e0420aa9a7c0dea40632fb
JAKLI <grot787898@gmail.com>
2017-06-27 15:14:17 +0300

    Add support “transaction per web request”

--- Some/Controllers/SomeActionInvoker.cs
+++ Some/Controllers/SomeActionInvoker.cs
@@ -1,16 +1,37 @@
 ﻿using System;
+using System.Data.Entity;
 using System.Web.Mvc;
 
 namespace Some.Controllers
 {
     public class SomeActionInvoker:ControllerActionInvoker
     {
+        private DbContext _context;
+
+        public SomeActionInvoker(DbContext context)
+        {
+            _context = context;
+        }
+
         public override bool InvokeAction(ControllerContext controllerContext, string actionName)
         {
             if (actionName.Equals("AddToUser", StringComparison.CurrentCultureIgnoreCase))
             {
-                var result = base.InvokeAction(controllerContext, actionName);
-                return result;
+                var transaction = _context.Database.BeginTransaction();
+                using (transaction)
+                {
+                    try
+                    {
+                        var result = base.InvokeAction(controllerContext, actionName);               
+                        transaction.Commit();
+                        return result;
+                    }
+                    catch (Exception e)
+                    {
+                        transaction.Rollback();
+                        throw;
+                    }
+                }
             }
             if (actionName.Equals("Base",StringComparison.CurrentCultureIgnoreCase))
             {
